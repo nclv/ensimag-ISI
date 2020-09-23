dac8eac1f63476e4cfdd93500b68539b27d116d5
n0way <n0way@localhost.localdomain>
2020-09-23 12:03:25 +0200

    Rational

--- POO/Rational.java
+++ POO/Rational.java
@@ -9,52 +9,118 @@ public class Rational {
         this(0, 1);
     }
 
+    /**
+     * Initializes a newly created Rational with the value n/1.
+     * 
+     * @param numerator numerator
+     */
     public Rational(int numerator) {
         this(numerator, 1);
     }
 
+    /**
+     * Initializes a newly created Rational with the value num/denom, then
+     * simplifies this rational.
+     *
+     * @param numerator   numerator
+     * @param denominator denominator
+     * @throws ArithmeticException if denom is zero
+     */
     public Rational(int numerator, int denominator) {
         if (denominator == 0) {
             throw new ArithmeticException("Denominator is zero.");
         }
+        this.denominator = denominator;
+        this.numerator = numerator;
+        reduce();
+    }
+
+    private void reduce() {
         // reduce fraction
-        int g = gcd(numerator, denominator);
-        this.numerator = numerator / g;
-        this.denominator = denominator / g;
+        int g = Rational.gcd(this.numerator, this.denominator);
+        this.numerator /= g;
+        this.denominator /= g;
+        if (this.denominator < 0) {
+            this.numerator = -this.numerator;
+            this.denominator = -this.denominator;
+        }
+    }
+
+    /**
+     * Copy constructor: initializes the newly created Rational with the numerator
+     * and denominator of the argument.
+     */
+    public Rational(Rational other) {
+        this(other.numerator, other.denominator);
     }
 
     // return the numerator and denominator of (this)
-    public int numerator() {
+    public int getNumerator() {
         return this.numerator;
     }
 
-    public int denominator() {
+    public int getDenominator() {
         return this.denominator;
     }
 
-    public float getValue() {
-        return this.numerator / (float) this.denominator;
+    /**
+     * Sets the numerator, then simplifies the rational.
+     * @param n new numerator value
+     */	
+    public void setNum(int n) {
+        this.numerator = n;
+        reduce();
     }
 
-    // return string representation of (this)
-    public String toString() {
-        if (this.denominator == 1)
-            return this.numerator + "";
-        else
-            return this.numerator + "/" + this.denominator;
+    /**
+     * Sets the denominator, then simplifies the rational.
+     * @param denominator new denominator value
+     * @throws ArithmeticException if d is zero 
+     */	
+    public void setDenom(int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero...");
+        }
+        this.denominator = denominator;
+        reduce();
     }
 
-    // return (this * b), reduce overflow risk
-    public Rational times(Rational b) {
+    /**
+     * Converts this rational value to double.
+     * 
+     * @return the double floating point value closest to the rational value
+     *         represented by this rational.
+     */
+    public double getValue() {
+        return (double) this.numerator / this.denominator;
+    }
+
+    /**
+     * Multiplies this with b, then simplifies the rational reducing overflow risk.
+     */
+    public Rational mult(Rational b) {
         Rational a = this;
 
         // reduce p1/q2 and p2/q1, then multiply, where a = p1/q1 and b = p2/q2
         Rational c = new Rational(a.numerator, b.denominator);
         Rational d = new Rational(b.numerator, a.denominator);
+
         return new Rational(c.numerator * d.numerator, c.denominator * d.denominator);
         // OR
         // return new Rational(this.numerator * b.numerator, this.denominator *
         // b.denominator);
+    }
+
+    /**
+     * Multiplies two rationals, then returns the result.
+     * This a class method, used this way:
+     *     Rational res = Rational.mult(a, b); 
+     */
+    public static Rational mult(Rational a, Rational b) {
+    	Rational c = new Rational(a.numerator, b.denominator);
+        Rational d = new Rational(b.numerator, a.denominator);
+
+        return new Rational(c.numerator * d.numerator, c.denominator * d.denominator);
     }
 
     // return (this + b)
@@ -71,11 +137,22 @@ public class Rational {
 
     // return (this / b)
     public Rational divides(Rational b) {
-        return this.times(b.reciprocal());
+        return this.mult(b.reciprocal());
     }
 
-    public boolean equals(Rational that) {
-        return this.numerator == that.numerator && this.denominator == that.denominator;
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof Rational) && (((Rational) o).getNumerator() == this.getNumerator())
+                && (((Rational) o).getDenominator() == this.getDenominator());
+    }
+
+    /** return string representation of (this) */
+    @Override
+    public String toString() {
+        if (this.denominator == 1)
+            return this.numerator + "";
+        else
+            return this.numerator + "/" + this.denominator;
     }
 
     // return gcd(|m|, |n|)
--- POO/TestRational.java
+++ POO/TestRational.java
@@ -17,18 +17,33 @@ public class TestRational {
         // 4/17 * 7/3 = 28/51
         x = new Rational(4, 17);
         y = new Rational(7, 3);
-        z = x.times(y);
+        z = x.mult(y);
         System.out.println(z);
 
         // 203/16957 * 9299/5887 = 17/899
         x = new Rational(203, 16957);
         y = new Rational(9299, 5887);
-        z = x.times(y);
+        z = x.mult(y);
         System.out.println(z);
 
         // 0/6 = 0
         x = new Rational(0, 6);
         System.out.println(x);
 
+        Rational r = new Rational(6, 4);
+        System.out.println("r = " + r);
+        System.out.println("\tas double, r = " + r.getValue());
+
+        Rational s = new Rational(2);
+        System.out.println("s = " + s);
+
+        r = r.add(s);
+        System.out.println("r + s = " + r);
+        
+        Rational t = new Rational(34, 8);
+        System.out.println("t = " + t + " (17/4 if reduced)");
+        
+        Rational st = Rational.mult(s, t);
+        System.out.println("st = " + st + " (17/2 if reduced)");
     }
 }
