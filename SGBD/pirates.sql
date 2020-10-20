21db71c58e8b9459f3e5a2ebbb51f78194e19900
Nicolas Vincent <nicolas.vincent100@gmail.com>
2020-10-20 11:00:33 +0200

    sql

---
+++ SGBD/pirates.sql
@@ -0,0 +1,52 @@
+create table navires (
+    CodeNav int PRIMARY KEY,
+    RayonAction float not null check(RayonAction >= 0.0),
+    NbPilotes int not null check(NbPilotes > 0),
+    Vmax float not null check(Vmax > 0.0)
+);
+
+create table pilotes (
+    CodeP int PRIMARY KEY,
+    NomPil char not null,
+    PrenomPil char not null,
+    Age int not null check(Age >= 18),
+    Grade char not null
+);
+
+create table typeMission (
+    TypeMis char check(TypeMis in ('transport', 'combat', 'interception', 'pillage')) PRIMARY KEY
+)
+
+create table missions (
+    CodeMis int PRIMARY KEY,
+    DateMis DATE not null,
+    Vmin float not null check(Vmin > 0.0),
+    RayonEng float not null check(RayonEng > 0.0),
+    NbNav int not null check(NbNav > 0),
+    TypeMis char not null references typeMission(TypeMis)
+);
+
+create table equipage(
+    CodeEq int PRIMARY KEY,
+    Effectif int not null check(Effectif > 0)
+);
+
+create table milieu (
+    Composition char check(Composition in ('acide', 'basique', 'neutre')) PRIMARY KEY
+);
+
+create table galaxies (
+  CodeGal int PRIMARY KEY,
+  NomGalaxie char not null,
+  Distance int not null check(Distance > 0)
+);
+
+create table planetes (
+    CodeGal int not null references galaxies(CodeGal),
+    CodePla int not null,
+    NomPla char not null,
+    Vlib float not null check(Vlib > 0.0),
+    Statut char not null check(Statut in ('non-exploree', 'reconnue', 'integree')),
+    Composition char not null references milieu(Composition),
+    PRIMARY KEY (CodeGal, CodePla)
+)
