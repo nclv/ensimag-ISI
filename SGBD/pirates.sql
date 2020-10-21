1799ff624c1e0db5503a9eedd5f657dc6bc5569b
n0way <n0way@localhost.localdomain>
2020-10-21 10:55:48 +0200

    SGBD sql

--- SGBD/pirates.sql
+++ SGBD/pirates.sql
@@ -49,4 +49,72 @@ create table planetes (
     Statut char not null check(Statut in ('non-exploree', 'reconnue', 'integree')),
     Composition char not null references milieu(Composition),
     PRIMARY KEY (CodeGal, CodePla)
-)
+);
+
+create table naviresTransport (
+    CodeNav int PRIMARY KEY references navires(CodeNav),
+    RayonAction float not null,
+    NbPilotes int not null,
+    Vmax float not null,
+    Capacite float not null
+);
+
+create table naviresTransport (
+    CodeNav int PRIMARY KEY references navires(CodeNav),
+    RayonAction float not null,
+    NbPilotes int not null,
+    Vmax float not null,
+    Tmin float not null,
+    Tmax float not null
+);
+
+create table peutcibler (
+    CodeMis int PRIMARY KEY references missions(CodeMis), 
+    CodePla int not null
+    CodeGal int not null
+    foreign key (CodeGal, CodePla) references planetes(CodeGal, CodePla)
+);
+
+create table formepar (
+    CodeP int not null references pilotes(CodeP),
+    TypeMis char not null references typeMission(TypeMis),
+    PRIMARY KEY (CodeP, TypeMis)
+);
+
+create table peutremplir (
+    CodeEq int not null references equipage(CodeEq),
+    TypeMis char not null references typeMission(TypeMis),
+    PRIMARY KEY (CodeEq, TypeMis)
+);
+
+create table affectationequipage (
+    CodeNav int not null references navires(CodeNav),
+    CodeEq int not null references equipage(CodeEq),
+    CodeMis int not null references missions(CodeMis),
+    PRIMARY KEY (CodeNav, CodeEq, CodeMis)
+);
+
+create table affectationpilote (
+    CodeNav int not null references navires(CodeNav),
+    CodeP int not null references pilotes(CodeP),
+    CodeMis int not null references missions(CodeMis),
+    PRIMARY KEY (CodeNav, CodeP, CodeMis)
+);
+
+create table adaptea (
+    CodeNav int not null references navires(CodeNav),
+    Composition char not null references milieu(Composition),
+    PRIMARY KEY (CodeNav, Composition)
+);
+
+create table qualifiepour (
+    CodeNav int not null references navires(CodeNav),
+    CodeP int not null references pilotes(CodeP),
+    PRIMARY KEY (CodeNav, CodeP)
+);
+
+create table affectationnavires (
+    CodeNav int not null references navires(CodeNav),
+    CodeMis int not null references missions(CodeMis),
+    PRIMARY KEY (CodeNav, CodeMis)
+);
